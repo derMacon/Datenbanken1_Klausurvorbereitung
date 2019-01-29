c16479b6fadabd829b278bc2cfc17dab1ac2182d
derMacon <silash169@gmail.com>
2019-01-29 04:17:09 +0100

    Klausur berichtigt

--- silas/Klausuren/Klausur_WS1516/aufg2.sql
+++ silas/Klausuren/Klausur_WS1516/aufg2.sql
@@ -3,10 +3,10 @@ Welche Maschinen besitzt die Firma, die vor 2000 angeschafft wurden? Geben
 Sie bitte jeweils die Maschinen-Nummer, den Namen der Maschine und das An-
 schaffungsjahr aus. Sortieren Sie die Ausgabe absteigend nach Anschaffungsjahr.*/
 
-select mnr as MaschinenNr, name as Name, year(ansch_datum) as Anschaffungsjahr
+select mnr, name, ansch_datum
 from maschine 
 where year(ansch_datum) < 2000
-order by year(ansch_datum) DESC;
+order by ansch_datum DESC;
 
 
 /* 2 b)
@@ -20,8 +20,8 @@ Mitarbeiterin. */
 select a.name as Abteilung, p.name as Name, p.vorname as Vorname
 from personal as p 
 join abteilung as a using(abt_nr)
-where a.name = 'Verwaltung' or a.name = 'Projektierung'
-order by a.name, p.name, p.vorname ASC;
+where a.name in ('Verwaltung', 'Projektierung')
+order by a.name, p.name, p.vorname;
 
 
 /* 2 c)
@@ -31,10 +31,11 @@ MitarbeiterInnen ohne Prämie sollen dabei mit Prämienanzahl 0 auftreten. Das
 Ergebnis soll absteigend nach Anzahl der Prämien sortiert sein, bei gleicher An-
 zahl alphabetisch aufsteigend nach Name und Vorname. */
 
-select name as Name, vorname as Vorname, case when p_betrag is null then 1 else p_betrag end as Praemienanzahl
-from personal
-left join praemie using(pnr)
-order by Praemienanzahl desc, name asc, vorname asc; 
+select name as Name, vorname as Vorname, coalesce(count(pr.pnr), 0) as Praemienanzahl
+from personal as p
+left join praemie as pr using(pnr)
+group by p.pnr
+order by count(pr.pnr) desc, name, vorname; 
 
 
 /* 2 d)
@@ -90,6 +91,5 @@ from gehalt as x
 -- join gehalt as y on x.geh_stufe = y.geh_stufe
 join personal as p on x.geh_stufe = p.geh_stufe
 join abteilung as a on p.abt_nr = a.abt_nr
-where x.betrag = max(x.betrag) 
-;
+where x.betrag = max(x.betrag);
 
