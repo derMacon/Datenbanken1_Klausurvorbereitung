2f8da37b12e72ae76c00e5cfe3e95e62a9e255f7
derMacon <silash169@gmail.com>
2019-01-31 04:02:24 +0100

    WS1314 soweit erstmal fertig.

--- silas/Klausuren/Klausur_WS1314/aufg2.sql
+++ silas/Klausuren/Klausur_WS1314/aufg2.sql
@@ -52,7 +52,7 @@ FROM personal p, gehalt g
 WHERE p.geh_stufe=g.geh_stufe;
 
 
-/* Für wie viele Maschinen sind die Mitarbeiter verantwortlich?
+/* e) Für wie viele Maschinen sind die Mitarbeiter verantwortlich?
 Geben Sie bitte für jeden Mitarbeiter (Personalnummer, Vorname, Nachname)
 unter der Ueberschrift Anz-Maschinen an, für wie viele Maschinen er oder sie
 verantwortlich ist. Für Mitarbeiter, die für keine Maschine verantwortlich sind,
@@ -66,28 +66,58 @@ group by pnr
 order by 1; 
 
 
-/* Benutzen Sie bitte Unterabfragen und vermeiden Sie Joins: Welche Mitarbeiter
+/* f) Benutzen Sie bitte Unterabfragen und vermeiden Sie Joins: Welche Mitarbeiter
 (Personalnummer, Vorname, Nachname) haben die höchste Einzelprämie erhal-
 ten? */
 
+select pnr, vorname, name 
+from personal 
+where pnr = (
+	select pnr
+	from praemie 
+	where p_betrag = (
+		select max(p_betrag) 
+		from praemie
+	)
+);
 
-/* Welche konkrete Antwort liefert diese Anfrage? */
+
+/* Welche konkrete Antwort liefert diese Anfrage? 
+
+pnr | vorname | name
+168, Egon, Hahn
 
 
 /* Sind Ihre Unterabfragen korreliert oder unkorrelliert? Bitte begründen Sie Ihre
-Antwort. */
+Antwort. 
+
+Die Unterabfragen sind unkorreliert, da von den inneren kein Bezug auf die aeusseren 
+genommen wird. 
 
 
-/* Welche Mitarbeiter (Personalnummer, Vorname, Nachname) haben Prämien er-
+g) Welche Mitarbeiter (Personalnummer, Vorname, Nachname) haben Prämien er-
 halten? Jeder Mitarbeiter soll nur einmal genannt werden, selbst wenn er mehrere
 Prämien erhalten hat. */
 
-
-/* Welche konkrete Antwort liefert diese Anfrage? */
-
-/* Tragen Sie bitte die Antwort auf die folgenden Anfrage in die Tabelle ein: */
-
-/* Welche Frage wird mit der oberen Anfrage beantwortet? */
+select distinct pnr, vorname, name 
+from personal
+join praemie using (pnr); 
 
 
+/* Welche konkrete Antwort liefert diese Anfrage? 
 
+pnr | vorname | name 
+227, Walter, Wagner
+124, Richard, Meier
+234, August, Krohn
+168, Egon, Hahn
+
+Tragen Sie bitte die Antwort auf die folgenden Anfrage in die Tabelle ein: */
+
+SELECT DISTINCT pnr , vorname , name
+FROM personal JOIN kind USING ( pnr )
+WHERE abt nr='d13';
+
+/* Welche Frage wird mit der oberen Anfrage beantwortet? 
+
+Welche Mitarbeiter der Abteilung d13 haben Kinder? */
